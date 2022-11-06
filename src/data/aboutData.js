9db1a3f3e129adb6ff3a146c3ce85b0cbdfef064
Usama Yousuf <uyousuf69@gmail.com>
2022-11-06 23:17:09 +0500

    .

--- src/data/aboutData.js
+++
@@ -1,8 +0,0 @@
-export const aboutData = {
-  title: "Who am I? ",
-  description1:
-    "My name's Usama Yousuf. I'm a web designer and developer based in Karachi, Pakistan.",
-  description2:
-    "During the day I work as a Junior developer at a UK Based Company and in the evening I  work on freelance projects. I utilize time to built my personal projects innovatively. Right now I'm also trying a hand at Devops. I love to learn and explore new arenas.",
-  image: 2,
-};
--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-  email: "uyousuf69@gmail.com",
-  phone: "+923368930890",
-  address: "C-36 Block 10-A Gulshan-e-Iqbal, Karachi",
-
-  sheetAPI: "",
-};
